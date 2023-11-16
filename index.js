7a98e17d6ce95dc02a284d5fece34e1cd8abac5f
Sanad-Alshobaki <alshobakisanad@gmail.com>
2023-11-16 04:21:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 class Formatter {
-  //add static methods here
+
+  static capitalize(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  }
+
+  static sanitize(word) {
+    return word.replaceAll(/[^A-Za-z0-9 '-]/g, '');
+  }
+
+  static titleize(word) {
+    let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let result = []
+    let arrayOfWords = word.split(' ')
+    for (let i = 0; i < arrayOfWords.length; i++) {
+      if (i == 0) {
+        result.push(this.capitalize(arrayOfWords[i]))
+      } else {
+        if (exceptions.includes(arrayOfWords[i])) {
+          result.push(arrayOfWords[i])
+        } else {
+          result.push(this.capitalize(arrayOfWords[i]))
+        }
+
+      }
+    }
+    return result.join(" ");
+  }
 }
